545bf6a6d8ffcbeb93bf6e4dc490bfb22fe4cd61
Risha <browny1320@mail.com>
2022-10-02 21:50:49 +0300

    adedd new homework

---
+++ lec.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+# days = int(input())
+# if days == 6:
+  #  print('Да')
+# elif days == 7:
+  #  print('Да')
+# else:
+  #  print('Нет')
+
+
+# from random import randint
+
+
+#n = int (input()) 
+#for i in range(0, n):
+ # print((-3) ** i, end=''+ ', ')
+
+
+
+list = []
+n = int(input())
+for i in range(0, n):
+  print(i+1,(':'), 3*(i+1)+1, end = ',')
+
